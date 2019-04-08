81e3e77aa7e662fefbee54396e1dec6489f8104e
ARYENN <arjeneggebeen@live.nl>
2019-04-08 12:42:15 +0200

    Added IP default for local dev

--- core/api_vars.py
+++ core/api_vars.py
@@ -4,7 +4,7 @@ API_PORT = os.getenv('API_PORT', '80')
 API_IP = os.getenv('API_IP', '0.0.0.0')
 API_DEBUG = os.getenv('API_DEBUG', True)
 
-DB_IP = os.getenv('DB_IP', '192.168.135.134')
+DB_IP = os.getenv('DB_IP', '192.168.37.50')
 DB_PORT = os.getenv('DB_PORT', '5432')
 DB_USER = os.getenv('DB_USER', 'postgres')
 DB_PASSWORD = os.getenv('DB_PASSWORD', 'password')
